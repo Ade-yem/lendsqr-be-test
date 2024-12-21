db1d90d8026a2ef6b17f184f2f5bd088fe2e4b75
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2024-12-21 15:33:06 +0100

    Removed validation middleware

--- src/route/accountRouter.ts
+++ src/route/accountRouter.ts
@@ -5,7 +5,7 @@ import { validateDeleteAccount } from "../validators/accountValidators";
 
 const accountRouter = Router();
 accountRouter.get("/get-account/:id", AuthController.verifyToken, AccountController.getAccount);
-accountRouter.delete("/delete-account/:id", AuthController.verifyToken, validateDeleteAccount, AccountController.deleteAccount);
+accountRouter.delete("/delete-account/:id", AuthController.verifyToken, AccountController.deleteAccount);
 accountRouter.post("/create-account", AuthController.verifyToken, AccountController.createAccount);
 accountRouter.post("/fund-account", AuthController.verifyToken, AccountController.fundAccount);
 accountRouter.post("/withdraw", AuthController.verifyToken, AccountController.checkIfYouOwnAccount, AccountController.withdrawFunds);
